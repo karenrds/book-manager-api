b735f0dce763154f0eef839faeb1054d70e338f1
karenrds <karen.rodrigues30@gmail.com>
2022-02-23 09:49:52 +0000

    Implemented exception handling

--- src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
+++ src/main/java/com/techreturners/bookmanager/controller/BookManagerController.java
@@ -1,5 +1,6 @@
 package com.techreturners.bookmanager.controller;
 
+import com.techreturners.bookmanager.exception.ApiRequestException;
 import com.techreturners.bookmanager.model.Book;
 import com.techreturners.bookmanager.service.BookManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,27 +26,43 @@ public class BookManagerController {
 
     @GetMapping({"/{bookId}"})
     public ResponseEntity<Book> getBookById(@PathVariable Long bookId) {
-        return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        try {
+            return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ApiRequestException("Requested Book is not available");
+        }
     }
 
     @PostMapping
     public ResponseEntity<Book> addBook(@RequestBody Book book) {
-        Book newBook = bookManagerService.insertBook(book);
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
-        return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
+        try {
+            Book newBook = bookManagerService.insertBook(book);
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("book", "/api/v1/book/" + newBook.getId().toString());
+            return new ResponseEntity<>(newBook, httpHeaders, HttpStatus.CREATED);
+        } catch (Exception e) {
+            throw new ApiRequestException("Invalid JSON");
+        }
     }
 
     //User Story 4 - Update Book By Id Solution
     @PutMapping({"/{bookId}"})
     public ResponseEntity<Book> updateBookById(@PathVariable("bookId") Long bookId, @RequestBody Book book) {
-        bookManagerService.updateBookById(bookId, book);
-        return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        try {
+            bookManagerService.updateBookById(bookId, book);
+            return new ResponseEntity<>(bookManagerService.getBookById(bookId), HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ApiRequestException("Requested book is not available");
+        }
     }
 
     @DeleteMapping({"/{bookId}"})
-    public ResponseEntity<String> deleteById(@PathVariable Long bookId) {
-        bookManagerService.deleteBookById(bookId);
-        return new ResponseEntity<String>("Deleted Book", HttpStatus.OK);
+    public ResponseEntity<Book> deleteBookById(@PathVariable Long bookId) {
+        try {
+            bookManagerService.deleteBookById(bookId);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception e) {
+            throw new ApiRequestException("Requested book is not available");
+        }
     }
 }
